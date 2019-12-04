f48b84f920e96a96594b5f63b4471a2be5db1cfb
TommyCakes <tomhwarhurst@gmail.com>
2019-12-04 14:30:30 +0000

    create basic layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import './App.scss';
+import gameImg from './images/games.png';
+
+const gamePlaceholder = {
+  background: `url(${gameImg})`,
+  backgroundSize: 'contain',
+}
 
 function App() {
   return (
     <div className="App">
       <div className="main-container">
-        <div className="child"></div>
-        <div className="child"></div>
-        <div className="child"></div>
-        <div className="child"></div>
+        {/* First row */}
+        <div className="tall">
+          <p>Weather</p>
+        </div>
+
+        <div className="inner-container">          
+          <div className="wide pink">
+          <p>Music</p>
+          </div>
+          <div className="child">
+            <p>Date / Time</p>
+          </div>  
+        </div>      
+                               
+        {/* Second Row */}        
+        <div className="inner-container">
+          <div className="child child-long">
+          <p>Twitch Search</p>
+          </div>            
+          <div style={gamePlaceholder} className="wide">
+          
+          </div>
+        </div> 
+
       </div>
     </div>
   );
