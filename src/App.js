a0af87780ac82247bfdde455d963f8a94e25b3ba
TommyCakes <tomhwarhurst@gmail.com>
2019-12-04 15:57:37 +0000

    created component files

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
 import React from 'react';
 import './App.scss';
-import gameImg from './images/games.png';
-
-const gamePlaceholder = {
-  background: `url(${gameImg})`,
-  backgroundSize: 'contain',
-}
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
-      <div className="main-container">
-        {/* First row */}
-        <div className="tall">
-          <p>Weather</p>
-        </div>
-
-        <div className="inner-container">          
-          <div className="wide pink">
-          <p>Music</p>
-          </div>
-          <div className="child">
-            <p>Date / Time</p>
-          </div>  
-        </div>      
-                               
-        {/* Second Row */}        
-        <div className="inner-container long-inner">
-          <div className="child child-long">
-          <p>Twitch Search</p>
-          </div>            
-          <div style={gamePlaceholder} className="wide wide-tv">
-          
-          </div>
-        </div> 
-
-      </div>
+      <Main />
     </div>
   );
 }
---
+++ src/Components/Main.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import gameImg from '../images/games.png';
+
+const gamePlaceholder = {
+    background: `url(${gameImg})`,
+    backgroundSize: 'contain',
+}
+
+export default class Main extends Component {
+    constructor(props) {
+        super(props);  
+        this.state = {
+            time: ""
+        }      
+    }    
+
+    render() {
+        return (
+            <div className="main-container">
+                {/* First row */}
+                <div className="tall">
+                <p>Weather</p>
+                </div>
+
+                <div className="inner-container">          
+                <div className="wide pink">
+                <p>Music</p>
+                </div>
+                <div className="child">
+                    <p>Date / Time</p>
+                </div>  
+                </div>      
+                                    
+                {/* Second Row */}        
+                <div className="inner-container long-inner">
+                <div className="child child-long">
+                <p>Twitch Search</p>
+                </div>            
+                <div style={gamePlaceholder} className="wide wide-tv">
+                
+                </div>
+                </div> 
+
+            </div>
+        )
+    }
+}
