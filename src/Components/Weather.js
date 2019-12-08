c22a9d519db5ca5f93b574429a380f58239baa6a
TommyCakes <tomhwarhurst@gmail.com>
2019-12-08 21:27:12 +0000

    color change according to temp

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import request from 'superagent';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloudRain } from '@fortawesome/free-solid-svg-icons';
+import { faCloudMoonRain } from '@fortawesome/free-solid-svg-icons';
 
 export default class Weather extends Component {
   constructor(props) {
@@ -18,19 +18,24 @@ export default class Weather extends Component {
 
   getWeatherForLocation() {
     let apiKey = "8955ed88a3c211ccce8222a9866954f3";
-    let city = "London";
+    let city = "Rio De Janeiro";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-    request.get(url, (err, res) => {
+    request.get(url, (err, res) => {        
       console.log(res.body);       
+      let { name, weather, icon, main} = res.body;
       this.setState({
-        location: res.body.name,
-        weather: res.body.weather[0].description || res.body.weather[1].description ,
-        temp: res.body.main.temp_max,
-        icon: res.body.weather[0].main || res.body.weather[1].main
+        location: name,
+        weather: weather[0].description,
+        temp: main.temp_max,
+        icon: weather[0].main,
       })                
     });
   }
- 
+  
+  setHotOrColdTemperature = () => {
+    return this.state.temp >= 18 ? 'hot-temp' : 'cold-temp';
+  }
+
   render() {
 
     let icon = '';
@@ -41,19 +46,19 @@ export default class Weather extends Component {
 
     return (
       <div className="weather">
-        <p className="weather-location">
-        The weather in {this.state.location}
-        </p>
+        <h1 className="weather-location">
+        The weather currently in {this.state.location}
+        </h1>
         <hr/>
-        <p className='weather-temperature'>
+        <p className={`weather-temperature  ${this.setHotOrColdTemperature()}`}>
         {this.state.temp} <span className="metric">℃</span>
         </p>
         
-
-        <p className='weather-description'>{this.state.weather}</p>
+        <hr/>
+        <p className='weather-description'>Looks like there is {this.state.weather} today</p>
 
         <div className="weather-logo">
-          <FontAwesomeIcon icon={faCloudRain} size="4x" color="white" /> 
+          <FontAwesomeIcon icon={faCloudMoonRain} size="4x" color="white" /> 
         </div>                        
       </div>            
     );
